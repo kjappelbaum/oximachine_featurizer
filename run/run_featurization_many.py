969f57f9d6e7671167ce7846b9d88bb39666f700
Kevin <kevin.jablonka@epfl.ch>
2020-04-15 08:57:11 +0200

    featurization_many

--- run/run_featurization_many.py
+++ run/run_featurization_many.py
@@ -14,35 +14,33 @@ from pathlib import Path
 from tqdm import tqdm
 from mine_mof_oxstate.featurize import GetFeatures
 
-OUTDIR = '/home/kevin/Dropbox/proj62_guess_oxidation_states/check_new_release/featurization'
-CSDDIR = '/home/kevin/Dropbox/proj62_guess_oxidation_states/check_new_release/additions'
-ALREADY_FEAUTRIZED = [Path(p).stem for p in glob(os.path.join(OUTDIR, '*.pkl'))]
-NAME_LIST = '/home/kevin/Dropbox/proj62_guess_oxidation_states/check_new_release/new_names_w_ox.pkl'
+
+OUTDIR = "/scratch/kjablonk/oximachine_all"
+INDIR = "/work/lsmo/jablonka/2020-4-7_all_csd_for_oximachine/cif_for_feat"
+ALREADY_FEAUTRIZED = [Path(p).stem for p in glob(os.path.join(OUTDIR, "*.pkl"))]
 
 
 def load_pickle(f):  # pylint:disable=invalid-name
-    with open(f, 'rb') as fh:  # pylint:disable=invalid-name
+    with open(f, "rb") as fh:  # pylint:disable=invalid-name
         result = pickle.load(fh)
     return result
 
 
-HAS_OX_NUMER = load_pickle(NAME_LIST)
-
-
 def featurize_single(structure, outdir=OUTDIR):
     if Path(structure).stem not in ALREADY_FEAUTRIZED:
-        if Path(structure).stem in HAS_OX_NUMER:
-            gf = GetFeatures.from_file(structure, outdir)  # pylint:disable=invalid-name
-            gf.run_featurization()
+        gf = GetFeatures.from_file(structure, outdir)  # pylint:disable=invalid-name
+        gf.run_featurization()
 
 
 def main():
-    all_structures = glob(os.path.join(CSDDIR, '*.cif'))
-    with concurrent.futures.ProcessPoolExecutor(max_workers=8) as executor:
-        for _ in tqdm(executor.map(featurize_single, all_structures), total=len(all_structures)):
+    all_structures = glob(os.path.join(INDIR, "*.cif"))
+    with concurrent.futures.ProcessPoolExecutor() as executor:
+        for _ in tqdm(
+            executor.map(featurize_single, all_structures), total=len(all_structures)
+        ):
             pass
 
 
-if __name__ == '__main__':
-    print(('working in {}'.format(CSDDIR)))
+if __name__ == "__main__":
+    print(("working in {}".format(INDIR)))
     main()  # pylint: disable=no-value-for-parameter
---
+++ run/submit_slurm_from_folder.py
@@ -0,0 +1,88 @@
+# -*- coding: utf-8 -*-
+# pylint:disable = logging-format-interpolation
+"""
+Status: Dev
+This runscript can be used to submit the featurizations on a HPC clusters with the SLURM workload manager.
+
+Usage: Install script in conda enviornment called ml on cluster and then run it using the
+the outdir, start and end indices and submit flag.
+"""
+from __future__ import absolute_import
+import os
+import pickle
+import time
+import logging
+from glob import glob
+from pathlib import Path
+import subprocess
+import click
+
+featurizer = logging.getLogger("featurizer")  # pylint:disable=invalid-name
+featurizer.setLevel(logging.DEBUG)
+logging.basicConfig(
+    filename="featurizer.log", format="%(filename)s: %(message)s", level=logging.DEBUG
+)
+
+THIS_DIR = os.path.dirname(__file__)
+
+OUTDIR = "/scratch/kjablonk/oximachine_all"
+INDIR = "/work/lsmo/jablonka/2020-4-7_all_csd_for_oximachine/cif_for_feat"
+ALREADY_FEAUTRIZED = [Path(p).stem for p in glob(os.path.join(OUTDIR, "*.pkl"))]
+
+
+SUBMISSION_TEMPLATE = """#!/bin/bash -l
+#SBATCH --chdir ./
+#SBATCH --mem       5GB
+#SBATCH --ntasks    1
+#SBATCH --cpus-per-task    1
+#SBATCH --job-name  {name}
+#SBATCH --time      0:10:00
+#SBATCH --partition=serial
+
+source /home/kjablonk/anaconda3/bin/activate
+conda activate ml
+
+run_featurization {structure} {outdir}
+"""
+
+all_structures = sorted(glob(os.path.join(INDIR, "*.cif")))
+
+
+def write_and_submit_slurm(workdir, name, structure, outdir, submit=False):
+    """writes a slurm submission script and submits it if requested"""
+    submission_template = SUBMISSION_TEMPLATE.format(
+        name=name + "_featurize", structure=structure, outdir=outdir
+    )
+    with open(
+        os.path.join(workdir, name + ".slurm"), "w"
+    ) as fh:  # pylint:disable=invalid-name
+        for line in submission_template:
+            fh.write(line)
+
+    featurizer.info("prepared {} for submission".format(name))
+    if submit:
+        subprocess.call("sbatch {}".format("{}.slurm".format(name)), shell=True)
+        time.sleep(2)
+        featurizer.info("submitted {}".format(name))
+
+
+@click.command("cli")
+@click.argument("outdir")
+@click.argument("start")
+@click.argument("end")
+@click.option("--submit", is_flag=True, help="actually submit slurm job")
+def main(outdir, start, end, submit):
+    """Runs the CLI"""
+    if not os.path.exists(outdir):
+        os.mkdir(outdir)
+
+    start = int(start)
+    end = int(end)
+    for structure in all_structures[start:end:5]:
+        if structure not in ALREADY_FEAUTRIZED:
+            name = Path(structure).stem
+            write_and_submit_slurm(THIS_DIR, name, structure, outdir, submit)
+
+
+if __name__ == "__main__":
+    main()  # pylint:disable=no-value-for-parameter
